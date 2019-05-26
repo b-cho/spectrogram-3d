823469fef130efd1e03fca791f47d9fdcbf4fd28
bcho04 <brandon.cho@gmail.com>
2019-05-26 12:24:41 -0700

    Updated DFT for Nyquist frequency, added plotting function

--- python/ft.py
+++ python/ft.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def sine_hz(fs, duration, f):
     # fs: sample rate (Hz, int)
@@ -7,22 +8,41 @@ def sine_hz(fs, duration, f):
     samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
     return samples
 
-def dft(x, N=None, abs=True):
+def dft(x, N=None, rl=True):
     if(not N):
         N = len(x)
 
     X = []
-    for k in range(N):
+    for k in range(int(N/2)): # Accounting for the Nyquist frequency and DFT folding
         X.append(np.sum([x[n] * np.exp((1j * -2 * np.pi * k * n) / N) for n in range(N)]))
-    if(abs):
+    if(rl):
         X = np.abs(X)
     return X
 
-def stft(x, nfft, overlap=128, sample_rate=44100):
+def stft(x, nfft, sample_rate, overlap=None):
     STFT = []
+    if(overlap == None):
+        overlap = int(nfft/2)
+
     for n in range(0, len(x), nfft):
         x_trim = x[max(0, n-overlap):max(0, n-overlap)+nfft] # Use a rectangular window
         dft_x = dft(x_trim)
         STFT.append(dft_x)
     
-    return STFT
+    return np.swapaxes(np.array(STFT), 0, 1), len(x) # The array is vertical, not horizontal.
+
+def show_spectrogram(S, sample_rate=None, n_samples=None):
+    fig = plt.figure(figsize=(8,7))
+    plt.imshow(S, origin="lower")
+
+    plt.xlabel("Time (sec)")
+    if(sample_rate and n_samples):
+        x_labels = np.round(np.linspace(0, (n_samples/sample_rate), 8), 1)
+    plt.xticks(np.round(np.linspace(0, S.shape[1], 8), 1), x_labels)
+
+    plt.ylabel("Frequency (Hz)")
+    if(n_samples and sample_rate):
+        y_labels = np.round(np.linspace(0, sample_rate/2, 8), 1)
+    plt.yticks(np.round(np.linspace(0, S.shape[0], 8), 1), y_labels)
+
+    plt.show()
